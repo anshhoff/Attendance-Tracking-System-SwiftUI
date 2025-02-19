f8b8fdfb641262fb15de4fd37de325cf410a6397
Ansh Hardaha <anshh2488@gmail.com>
2025-02-19 12:29:09 +0530

    Initial Commit

---
+++ AttendanceTracker/AttendanceTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  AttendanceTrackerApp.swift
+//  AttendanceTracker
+//
+//  Created by Ansh Hardaha on 2025/02/19.
+//
+
+import SwiftUI
+
+@main
+struct AttendanceTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
